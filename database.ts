f8c7de87c8847c97ae5012332da32ba5e44ca40b
Lawrence Ephrim <ephrimlawrence0@gmail.com>
2025-01-07 05:17:46 +0000

    feat(crawler): read db url from env

--- database.ts
+++ database.ts
@@ -1,6 +1,7 @@
 import { type Db, MongoClient } from "mongodb";
 
-const client = new MongoClient("mongodb://localhost:27017")
+require('dotenv').config()
+const client = new MongoClient(process.env.DB_URL || "mongodb://localhost:27017")
 export let db: Db;
 
 
